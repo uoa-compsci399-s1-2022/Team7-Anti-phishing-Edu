ccdc42d00b1b1ddde5639f1e1081bbf3d9b2b276
kanericky <kane-ricky@outlook.com>
2022-06-06 16:27:32 +0800

    06/06/2022 Commit

--- Assets/Scripts/General/LevelManager01.cs
+++ Assets/Scripts/General/LevelManager01.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class LevelManager01 : MonoBehaviour
 {
@@ -9,11 +11,18 @@ public class LevelManager01 : MonoBehaviour
     public GameObject videoPanel;
     public GameObject tips;
 
+    public string fileName;
+    public VideoPlayer videoPlayer;
+
     private void Start()
     {
         defaultButton.SetActive(false);
         returnButton.SetActive(false);
         StartCoroutine(Delay());
+
+        videoPlayer.url = Application.streamingAssetsPath + "/" + fileName;
+        //Debug.Log(Application.streamingAssetsPath + "/" + fileName);
+        videoPlayer.Play();
     }
 
     private IEnumerator Delay()
